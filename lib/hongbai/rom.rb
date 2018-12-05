4c4232819267ac1d62e5b825fb2b6df2566f74a1
Thomas Lin <dkeehl@gmail.com>
2018-12-05 09:51:34 +0800

    add mapper 0

--- lib/hongbai/rom.rb
+++ lib/hongbai/rom.rb
@@ -61,6 +61,8 @@ module Hongbai
       "TV System: #{@tv_system}\n"
     end
 
+    # mapper 0
+
     private
       def self.new(prg_rom, chr_rom, trainer, mapper, mirroring, tv_system)
         super
@@ -73,6 +75,19 @@ module Hongbai
         @mapper = mapper
         @mirroring = mirroring
         @tv_system = tv_system
+
+        make_mapper
+      end
+
+      def make_mapper
+        # Only support mapper0 at the moment
+        mapper = case @mapper
+                 when 0 then Nrom
+                 else
+                   raise "Unsupported mapper"
+                 end
+        singleton_class.class_eval { include mapper }
+        mapper_init
       end
   end
 
@@ -88,5 +103,28 @@ module Hongbai
     class FourScreens < self
       def self.to_s; "FourScreens" end
     end
+  end
+
+  # Mapper 0
+  module Nrom
+    def mapper_init
+      @prg_addr_mask = @prg_rom.length > 16384 ? 0x7ffff : 0x3ffff
+    end
+
+    def next_scanline_irq; false end
+
+    def prg_load(addr)
+      addr < 0x80000 ? 0 : @prg_rom[addr & @prg_addr_mask]
+    end
+
+    # Read only
+    def prg_set(_addr, _val); end
+
+    def chr_load(addr)
+      @chr_rom[addr]
+    end
+
+    # Read only
+    def chr_set(_addr, _val); end
   end
 end
