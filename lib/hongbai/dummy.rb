37214c1eb6a16a781c702f8f8172784f27b16640
Thomas Lin <dkeehl@gmail.com>
2018-12-31 15:30:05 +0800

    update dummy

--- lib/hongbai/dummy.rb
+++ lib/hongbai/dummy.rb
@@ -46,11 +46,11 @@ module Hongbai
     class Input
       def poll; end
       
-      def read_4016; 0 end
+      def read_4016(_addr); 0x40 end
 
-      def read_4017; 0 end
+      def read_4017(_addr); 0x40 end
 
-      def store(_); end
+      def write_4016(_addr, _val); end
     end
   end
 end
