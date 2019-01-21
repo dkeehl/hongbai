6617ffe744f971bd5f402d1186fc2b897e9771d5
Thomas Lin <dkeehl@gmail.com>
2019-01-21 16:34:42 +0800

    update arity

--- lib/hongbai/mappers/template.rb
+++ lib/hongbai/mappers/template.rb
@@ -1,19 +1,19 @@
 module Hongbai
   module Mapper
     # Setup
-    def mapper_init; end
+    def mapper_init(_prg_rom, _chr_rom); end
 
     # Returns a pattern table
     # A pattern table is an object which has a method #[] : Interger -> Array
-    # the integer is in range (0..(511 * 8)), the array is of size 8.
+    # the integer is in range (0..0x1000), the array is of size 8.
     def pattern_table; end
 
-    def prg_read_method; end
+    def prg_read_method(_addr); end
 
-    def prg_write_method; end
+    def prg_write_method(_addr); end
 
-    def chr_read_method; end
+    def chr_read_method(_addr); end
 
-    def chr_write_method; end
+    def chr_write_method(_addr); end
   end
 end
